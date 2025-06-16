8e332c03690dc1de775ba398cd9906dd45f11775
AnshSoniWebEarl <anshsoni.webearl@gmail.com>
2025-06-16 15:13:04 +0530

    3002 port added

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(cors({
   origin: [
     'http://localhost:3000',
     'http://localhost:3001',
+    'http://localhost:3002',
     'https://3-space.vercel.app',
     'https://threespacebackend.onrender.com',
     'https://www.3spacecorp.com',
